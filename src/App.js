aee4e8c00633b744ee564f01d63ff7800b304140
62630619 <sumanadasrh@gmail.com>
2024-10-23 17:59:36 +0530

    Frontend Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// import "./App.css";
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
